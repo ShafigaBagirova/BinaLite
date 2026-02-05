477453847393cece18453ae58c999cdbe82cd24b
ShafigaBagirova <bagiroffa06@gmail.com>
2026-02-05 23:12:27 +0400

    MinioServicesAdded Commit

---
+++ Src/Persistence/Repositories/PropertyMediaRepository.cs
@@ -0,0 +1,22 @@
+﻿using Application.Abstracts.Repositories;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Context;
+
+namespace Persistence.Repositories;
+
+public class PropertyMediaRepository : GenericRepository<PropertyMedia, int>, IPropertyMediaRepository
+{
+    private readonly BinaLiteDbContext _context;
+    public PropertyMediaRepository(BinaLiteDbContext context) : base(context)
+    {
+        _context = context;
+    }
+    public Task<List<PropertyMedia>> GetByPropertyAdIdAsync(int propertyAdId, CancellationToken ct = default)
+    {
+        return _context.Set<PropertyMedia>()
+           .Where(x => x.PropertyAdId == propertyAdId)
+           .OrderBy(x => x.Order)
+           .ToListAsync(ct);
+    }
+}
